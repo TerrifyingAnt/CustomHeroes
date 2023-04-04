3eb49836d44d5d8c3f0542786915ed49b18a80df
TerrifyingAnt <TerrifyingAnt@yandex.ru>
2023-04-05 00:41:18 +0300

    Начальный коммит

---
+++ src/main/java/jg/actionfigures/server/ServerApplication.java
@@ -0,0 +1,13 @@
+package jg.actionfigures.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerApplication.class, args);
+	}
+
+}
