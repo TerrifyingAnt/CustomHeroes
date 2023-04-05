6e3f29dadfebc3ac81da424142636851ab0e228d
Ant <80172256+TerrifyingAnt@users.noreply.github.com>
2023-04-05 17:35:22 +0300

    Добавлен основной контроллер

---
+++ src/main/java/jg/actionfigures/server/Controllers/MainController.java
@@ -0,0 +1,16 @@
+package jg.actionfigures.server.Controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/test")
+public class MainController {
+
+    @GetMapping("/xd")
+    public String test() {
+        return "тест";
+    }
+    
+}
