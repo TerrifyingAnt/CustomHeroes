45082471152e50780e398b876398206b0c7dcd05
Ant <80172256+TerrifyingAnt@users.noreply.github.com>
2023-04-05 17:36:37 +0300

    Добавлена модель "Заказ"

---
+++ src/main/java/jg/actionfigures/server/Models/Order.java
@@ -0,0 +1,69 @@
+package jg.actionfigures.server.Models;
+
+import java.sql.Date;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="order_table")
+public class Order {
+
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    @Column(name="order_id")
+    private Integer id;
+
+    @OneToOne(cascade=CascadeType.ALL)
+    @JoinColumn(name="user_id")
+    private User user;
+
+    @Column(name="order_state")
+    private String state;
+
+    @Column(name="order_date")
+    private Date date;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    
+
+    
+}
