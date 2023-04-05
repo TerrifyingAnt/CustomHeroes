eaf4e47fbf4715079f74f17f05f60571e8520276
Ant <80172256+TerrifyingAnt@users.noreply.github.com>
2023-04-05 17:36:26 +0300

    Добавлена модель "Элемент заказа"

---
+++ src/main/java/jg/actionfigures/server/Models/OrderItem.java
@@ -0,0 +1,70 @@
+package jg.actionfigures.server.Models;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="order_item_table")
+public class OrderItem {
+    
+
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    @Column(name="order_item_id")
+    private Integer id;
+
+    @Column(name="order_item_count")
+    private Integer count;
+
+    @OneToOne(cascade=CascadeType.ALL)
+    @JoinColumn(name="order_id")
+    private Order order;
+
+    @OneToOne(cascade=CascadeType.ALL)
+    @JoinColumn(name="figure_id")
+    private Figure figure;
+
+    
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getCount() {
+        return count;
+    }
+
+    public void setCount(Integer count) {
+        this.count = count;
+    }
+
+    public Order getOrder() {
+        return order;
+    }
+
+    public void setOrder(Order order) {
+        this.order = order;
+    }
+
+    public Figure getFigure() {
+        return figure;
+    }
+
+    public void setFigure(Figure figure) {
+        this.figure = figure;
+    }
+
+    
+    
+    
+}
