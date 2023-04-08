a6f74c8067bdd7d4177b6f6bdd7b3f521cf0af97
Ant <80172256+TerrifyingAnt@users.noreply.github.com>
2023-04-08 14:03:32 +0300

    Добававлен репозиторий пользователя

---
+++ src/main/java/jg/actionfigures/server/API/UserRepository.java
@@ -0,0 +1,9 @@
+package jg.actionfigures.server.API;
+
+import org.springframework.data.repository.CrudRepository;
+
+import jg.actionfigures.server.Models.User;
+
+public interface UserRepository extends CrudRepository<User, Integer>{
+    public User findByLogin(String login);
+}
