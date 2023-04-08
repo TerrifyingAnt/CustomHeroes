ebd1df93476b8680844cddaef9652e250b08ea67
Ant <TerrifyingAnt@yandex.ru>
2023-04-09 01:47:51 +0300

    Настройки безопасности приложения

---
+++ src/main/java/jg/actionfigures/server/Config/SecurityConfig.java
@@ -0,0 +1,50 @@
+package jg.actionfigures.server.Config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import jakarta.servlet.Filter;
+
+
+@Configuration
+@EnableGlobalMethodSecurity(
+        prePostEnabled = true
+)
+public class SecurityConfig implements WebSecurityCustomizer {
+    
+    @Override
+    public void customize(WebSecurity web) {
+        web
+        .ignoring()
+        .requestMatchers("/resources/**")
+        .requestMatchers("/publics/**");
+    }
+    
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        JwtAuthenticationFilter jwtAuthenticationFilter = new JwtAuthenticationFilter();
+        http.addFilterBefore(jwtAuthenticationFilter, (Class<? extends Filter>) UsernamePasswordAuthenticationFilter.class);
+        
+        return http.authorizeHttpRequests()
+            .requestMatchers("/api/auth/register").permitAll()
+            .requestMatchers("/api/auth/login").permitAll()
+            .requestMatchers("/api/auth/logout").permitAll()
+            .requestMatchers("/test/xd").hasAnyRole("CUSTOMER")
+            .requestMatchers("/api/auth/users").hasAnyRole("ADMIN")
+            .and().csrf().disable()
+            .formLogin().disable()
+            .logout().disable()
+            .httpBasic().disable()
+            .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+            .and().build();
+    }
+}
